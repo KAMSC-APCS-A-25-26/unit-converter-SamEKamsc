7a80ad105265fe46cee1810f8ed096f86c4c3f70
SamEKamsc <03samuel.edmonds@kamsc.org>
2025-09-30 13:54:15 -0400

    not sure

--- src/main/java/TimeConverter.java
+++ src/main/java/TimeConverter.java
@@ -7,14 +7,12 @@ public class TimeConverter {
         while (running) {
             System.out.println("Choose time to total seconds");
             System.out.print("Enter hours: ");
-            double h = sc.nextInt();
-            System.out.println();
+            int h = sc.nextInt();
             System.out.print("Enter minutes: ");
-            double m = sc.nextInt();
-            System.out.println();
+            int m = sc.nextInt();
             System.out.print("Enter seconds: ");
-            double s = sc.nextInt();
-            System.out.println("\n");
+            int s = sc.nextInt();
+            System.out.println();
             System.out.println("Time Conversion:");
             System.out.println("Input: " + h + " hours, " + m + " minutes, " + s + " seconds");
             System.out.println("Total: " + (h*3600 + m*60 + s) + " seconds\n");
@@ -22,8 +20,12 @@ public class TimeConverter {
             System.out.println(h + " hours × 3600 = " + h*3600 + " seconds");
             System.out.println(m + " minutes × 60 = " + m*60 + " seconds");
             System.out.println(s + " seconds = " + s + " seconds");
-            
-
+            System.out.println("Total: " + (h*3600 + m*60 + s) +  " seconds\n");
+            System.out.print("Do you want to convert another time? (y/n):  ");
+            String cont = sc.nextLine();
+            if (cont.equals("n")) {
+                System.out.println("Goodbye!");
+                running = false;
             }
         }
         // TODO: Implement time converter
