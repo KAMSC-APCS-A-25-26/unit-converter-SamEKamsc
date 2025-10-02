b181d8db0a5ae211ce3648a0c0bf97acfc29b60e
SamEKamsc <03samuel.edmonds@kamsc.org>
2025-10-02 11:22:59 -0400

    rounding

--- src/main/java/DistanceConverter.java
+++ src/main/java/DistanceConverter.java
@@ -9,25 +9,25 @@ public class DistanceConverter {
             System.out.println("1. Convert Miles to Kilometers");
             System.out.println("2. Kilometers to Miles");
             System.out.println("3. Exit\n");
-            System.out.print("Enter your choice:  ");
+            System.out.print("Enter your choice (1, 2, or 3): ");
             int choice = sc.nextInt();
             System.out.println();
             switch (choice) {
                 case 1:
-                    System.out.print("Enter distance in miles:  ");
+                    System.out.print("Enter distance in miles: ");
                     double miles = sc.nextDouble();
                     System.out.println("\n");
                     System.out.println("Conversion Results:");
-                    System.out.println("Miles" + miles);
-                    System.out.println("Kilometers: " + miles*1.60935 + "\n");
+                    System.out.println("Miles: " + miles);
+                    System.out.printf("Kilometers: %.5f\n\n", miles*1.60935);
                     break;
                 case 2:
-                    System.out.print("Enter distance in kilometers:  ");
+                    System.out.print("Enter distance in kilometers: ");
                     double km = sc.nextDouble();
                     System.out.println("\n");
                     System.out.println("Conversion Results:");
                     System.out.println("Kilometers: " + km);
-                    System.out.println("Miles: " + km/1.60935 + "\n");
+                    System.out.printf("Miles: %.5f\n\n", km/1.60935);
                     break;
                 case 3:
                     System.out.println("Goodbye!");
